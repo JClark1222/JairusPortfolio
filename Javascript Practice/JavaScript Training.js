3681fe449641be4a4454409162ea935cf5e5e342
Jairus Clark <mlggeek@gmail.com>
2026-06-02 05:00:24 -0400

    HTML & JavaScript Practice

---
+++ Javascript Practice/JavaScript Training.js
@@ -0,0 +1,11 @@
+function agentNeon() {
+    document.getElementById("myImage").src = "https://i.pinimg.com/1200x/8e/1a/c2/8e1ac20ca6df1ea1b91c278bd738de5f.jpg"
+}
+
+function agentJett() {
+    document.getElementById('myImage').src = 'https://assetsio.gnwcdn.com/valorant-neon-splash.jpg?width=1200&height=900&fit=crop&quality=100&format=png&enable=upscale&auto=webp'
+}
+
+function reset() {
+    document.getElementById('myImage').src = 'https://i.ytimg.com/vi/8f-pJIgTwog/maxresdefault.jpg'
+}
