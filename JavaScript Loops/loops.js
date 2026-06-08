56abb3487fa2b6ccd8ca4711f911f33dd0678388
Jairus Clark <mlggeek@gmail.com>
2026-06-08 10:55:46 -0400

    Practicing JavaScript Loops

---
+++ JavaScript Loops/loops.js
@@ -0,0 +1,120 @@
+// Question 1 ChatGPT
+let text = "";
+
+for(let i = 5; i < 30; i += 5) {
+    console.log(i)
+}
+
+// Question 2 ChatGPT
+let text2 = "";
+
+for(let i = 20; i > 0; i -= 2) {
+    console.log(i)
+}
+
+// Question 3 ChatGPT
+let text3 = "";
+
+for(let i = 0; i < 11; i++) {
+    text3 += i
+}
+
+// Question 4 in ChatGPT
+const fruits = [
+    "Apple",
+    "Banana",
+    "Orange",
+    "Grape",
+    "Strawberry"
+];
+text4 = ""
+
+for(let i = 0; i < fruits.length; i++){
+    console.log(text4 += i)
+}
+
+// Question 5 in ChatGPT
+text5 = ""
+
+for(let i = 0; i < fruits.length; i++) {
+    if(fruits[i].tostring().startsWith("B")){
+        text5 += fuits[i]
+        break;
+    }
+    else{
+        continue
+    }
+}
+
+// Question 6 in ChatGPT
+text6 = ""
+
+for(let i = 0; i < fruits.length; i++) {
+    if(fruits[i] == "Strawberry"){
+        text6 += fuits[i]
+        break;
+    }
+    else{
+        continue
+    }
+}
+
+// Question 7 in ChatGPT
+const ages = [15, 21, 17, 30, 25, 13];
+let adult = 0;
+text7 =""
+
+for(let i = 0; i > ages.length; i++){
+    if(ages[i] >= 18){
+        adult += 1
+    } else {
+        continue
+    }
+    text7 += adult
+}
+
+// Question 8 in ChatGPT
+text8 =""
+
+for(let i = 0; i < ages.length; i++){
+    if(i < ages[i]){
+        text8 = i
+    } else {
+        continue
+    }
+}
+
+// Question 9 in ChatGPT
+"I don't know this one"
+
+// Question 10 in ChatGPT
+let tags = ""
+
+for(let i = 0; i < 6; i++){
+    tags += "#";
+    console.log(tags)
+}
+
+//Bonus Question in ChatGPT
+const games = [
+    "Valorant",
+    "Minecraft",
+    "Fortnite",
+    "Minecraft",
+    "Rocket League"
+];
+
+mineCraftCount = 0;
+countAnswer = 0;
+
+for(let i = 0; i < games.length; i++) {
+    if(games[i] == "Minecraft"){
+        mineCraftCount++
+    } else {
+        continue
+    }
+    countAnswer = mineCraftCount
+}
+
+
+
