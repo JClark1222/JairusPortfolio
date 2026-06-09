3d7e1608126f35d6e578326e10bc52fcdcf92d70
Jairus Clark <mlggeek@gmail.com>
2026-06-09 08:47:59 -0400

    Starting my BookMark project

---
+++ BookMark/index.js
@@ -0,0 +1,18 @@
+// This is the array that will hold the books
+const bookLib = []
+
+// Reset the form once submitted
+reset = () => document.getElementById("myForm").reset();
+
+// This function will push the user response into the array
+function saveBook() {
+
+    bookLib.push(document.getElementById("bookEntry").value)
+
+    for (book of bookLib) {
+        console.log(book)
+        document.getElementById("bookList").innerHTML = bookLib.map(book => `<li>${book}</li>`).join('');
+    }
+    
+    reset();
+}
