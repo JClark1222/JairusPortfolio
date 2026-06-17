9b50e9c14b392c83f1f95a28e08d6ac131acc303
Jairus Clark <mlggeek@gmail.com>
2026-06-17 04:04:52 -0400

    Starting a progress bar feature for currently reading

--- BookMark/index.js
+++ BookMark/index.js
@@ -1,5 +1,6 @@
 // This is the array that will hold the books
-const bookLib = []
+const favoriteReads = []
+const currentRead = []
 
 // Reset the form once submitted
 reset = () => document.getElementById("myForm").reset();
@@ -7,12 +8,41 @@ reset = () => document.getElementById("myForm").reset();
 // This function will push the user response into the array
 function saveBook() {
 
-    bookLib.push(document.getElementById("bookEntry").value)
+    favoriteReads.push(document.getElementById("bookEntry").value)
 
-    for (book of bookLib) {
-        console.log(book)
-        document.getElementById("bookList").innerHTML = bookLib.map(book => `<h3>${book}</h3>`).join('');
+    for (book of favoriteReads) {
+        document.getElementById("bookItem").innerHTML = favoriteReads.map((book, i) => `<button onclick="removeFav(${i})">X</button><h3>${book}</h3>`).join('');
     }
-
     reset();
+}
+
+function saveCurrentBook() {
+
+    currentRead.push(document.getElementById("currentEntry").value)
+
+    for (book of currentRead) {
+        document.getElementById("currentRead").innerHTML = currentRead.map((book, i) => `<button onclick="removeCurrent(${i})">X</button><h3>${book}</h3>`).join('');
+    }
+    reset();
+}
+
+//Remove an item from the list
+function removeFav(i) {
+    favoriteReads.splice(i,1)
+    
+        if (favoriteReads.length === 0 ) {
+            document.getElementById("bookItem").innerHTML = ""
+        } else {
+            document.getElementById("bookItem").innerHTML = favoriteReads.map(book => `<button onclick="removeFav()">X</button><h3>${book}</h3>`).join('');
+        }
+}
+
+function removeCurrent(i) {
+    currentRead.splice(i,1)
+    
+        if (currentRead.length === 0 ) {
+            document.getElementById("currentRead").innerHTML = ""
+        } else {
+            document.getElementById("currentRead").innerHTML = currentRead.map(book => `<button onclick="removeCurrent()">X</button><h3>${book}</h3>`).join('');
+        }
 }
